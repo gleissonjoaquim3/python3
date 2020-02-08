ed7f9e1a328d0a9b4ba08cc931eb3691b64b00bd
gleissonjoaquim3 <gleissoncg2@gmail.com>
2020-02-08 02:00:28 -0300

    Init

---
+++ exe1.py
@@ -0,0 +1,9 @@
+def soma(x, y):
+    """Soma 2 números """
+    return x + y
+
+
+x = int(input('Digite um número somavel : '))
+y = int(input('Digite um número somavel : '))
+
+print(f'Ola ! A soma é de {soma(x,y)} !')
