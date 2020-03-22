379f7271dd3be8a6470338a1da71645c5406a345
gleissonjoaquim3 <gleissoncg2@gmail.com>
2020-03-22 15:37:02 -0300

    update

--- Introdução_ao_Python/operadores/ex2_2.py
+++ Introdução_ao_Python/operadores/ex2_2.py
@@ -1,6 +1,5 @@
 # Calcula a soma de três variáveis e imprime o resultado na tela.
 
-
 a = 4
 b = 5
 c = 4
---
+++ Introdução_ao_Python/string/ex3_1.py
@@ -0,0 +1,6 @@
+# Operações com Strings
+
+# Concatenação
+s = 'ABC'
+t = s + 'C'
+print(t)  # Retorno com ABCC , soma (junta) duas string
