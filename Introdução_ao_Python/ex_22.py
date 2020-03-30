3489b2769285f04268203527fd318b77a4679295
gleissonjoaquim3 <gleissoncg2@gmail.com>
2020-03-30 01:34:44 -0300

    Mais exemplos emacs python
    Signed-off-by: gleissonjoaquim3 <gleissoncg2@gmail.com>

--- Introdução_ao_Python/ex_22.py
+++ Introdução_ao_Python/ex_22.py
@@ -1,8 +1,9 @@
 """Programa que faz cálculos núméricos"""
+
 primeiro = int(input('Diga o primeiro número: '))
 segundo = int(input('Diga o segundo número: '))
 escolha = int(
-    input('Escolha a operação: 1(soma),2(subtr),3(divis) e 4(multi):'))
+    input('Escolha a operação: 1(soma),2(subtr),3(divis) e 4(multi): '))
 calculo = 0
 if escolha == 1:
     escolha = 'Soma'
@@ -17,4 +18,4 @@ elif escolha == 4:
     escolha = 'Multiplicação'
     calculo = primeiro * segundo
 
-print(f'A {escolha} de {primeiro} com {segundo} é : {calculo}')
+print(f'A {escolha} de {primeiro} e {segundo} é : {calculo:.2f}')
